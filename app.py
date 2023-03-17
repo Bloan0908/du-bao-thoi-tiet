6ffa5ad381e9f8825516f6a5126027f12f96813c
Bloan0908 <125679333+Bloan0908@users.noreply.github.com>
2023-03-17 22:06:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pickle
 st.title('Revenue Prediction')
 a=st.number_input('Input Temperature')
+pickle.dump(model, open(filename, "wb"))
+model = pickle.load(open(filename, "rb"))
 if st.button('Predict'):
   st.write('Revenue Prediction',model.predict(a))
